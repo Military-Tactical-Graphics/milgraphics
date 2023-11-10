6700536cf3fb0c660d07036516456b2789e934e4
Martin Kučera <mkucera@retia.cz>
2023-11-10 09:07:51 +0100

    Fix labeledLine method

--- src/geometry/labeledLine.js
+++ src/geometry/labeledLine.js
@@ -21,25 +21,26 @@ export const labeledLine = (feature, text, uniqFirst = false) => {
     annotationUnder += `-${feature.properties.dtg1}`;
 
   geometry.coordinates = [points];
-  const A1 = ms.geometry.bearingBetween(points[0], points[1]) % 180 - 90;
+  const A1 = ms.geometry.bearingBetween(points[0], points[1]);
   const LAST_TWO = points.slice(-2);
-  const A2 = ms.geometry.bearingBetween(LAST_TWO[0], LAST_TWO[1]) % 180 - 90;
+  const A2 = ms.geometry.bearingBetween(LAST_TWO[0], LAST_TWO[1]);
 
   annotations.push(ms.geometry.addAnnotation(
-    ms.geometry.toDistanceBearing(points[0], 70, A1 + 45), annotationTop, { align: 'left', angle: A1 }));
+    ms.geometry.toDistanceBearing(points[0], 70, A1 > 180 ? A1 + 45 : A1 - 45), annotationTop, { align: A1 > 180 ? 'right' : 'left', angle: (A1 % 180) - 90 }));
   annotations.push(ms.geometry.addAnnotation(
-    ms.geometry.toDistanceBearing(LAST_TWO[1], 70, A1 - 45), annotationTop, { align: 'right', angle: A2 }));
+    ms.geometry.toDistanceBearing(LAST_TWO[1], 70, A2 > 180 ? A2 + 135 : A2 - 135), annotationTop, { align: A2 > 180 ? 'left' : 'right', angle: (A2 % 180) - 90 }));
 
   annotations.push(ms.geometry.addAnnotation(
-    ms.geometry.toDistanceBearing(points[0], -70, A1 - 45), annotationUnder, { align: 'left', angle: A1 }));
+    ms.geometry.toDistanceBearing(points[0], 70, A1 > 180 ? A1 - 45 : A1 + 45), annotationUnder, { align: A1 > 180 ? 'right' : 'left', angle: (A1 % 180) - 90  }));
   annotations.push(ms.geometry.addAnnotation(
-    ms.geometry.toDistanceBearing(LAST_TWO[1], -70, A2 + 45), annotationUnder, { align: 'right', angle: A2 }));
+    ms.geometry.toDistanceBearing(LAST_TWO[1], 70, A2 > 180 ? A2 - 135 : A2 + 135), annotationUnder, { align: A2 > 180 ? 'left' : 'right', angle: (A2 % 180) - 90 }));
+
 
   const TEXT = `PL ${feature.properties?.uniqueDesignation1 || ''}`
   annotations.push(ms.geometry.addAnnotation(
-    ms.geometry.toDistanceBearing(points[0], 70, A1 - 90), TEXT, { align: 'right', angle: A1 }));
+    ms.geometry.toDistanceBearing(points[0], 70, A1 > 180 ? A1 - 180 : A1 + 180), TEXT, { align: A1 > 180 ? 'left' : 'right', angle: A1 % 180 - 90 }));
   annotations.push(ms.geometry.addAnnotation(
-    ms.geometry.toDistanceBearing(LAST_TWO[1], 70, A2 + 90), TEXT, { align: 'left', angle: A2 }));
+    ms.geometry.toDistanceBearing(LAST_TWO[1], 70, A2), TEXT, { align: A2 > 180 ? 'right' : 'left', angle: A2 % 180 - 90 }));
 
   return { geometry: geometry, annotations: annotations };
 }
